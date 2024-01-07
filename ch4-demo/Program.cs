7f49fc2c0102dc846020147acb2bdd92ecedce51
chuangmaster <chuang.master@gmail.com>
2024-01-07 14:50:33 +0800

    di injection v1

--- ch4-demo/Program.cs
+++ ch4-demo/Program.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
 
 var porvider = new ServiceCollection()
                 .AddTransient<IFoo, Foo>()
-                .AddScoped<IFoo, Foo>()
-                .AddSingleton<IFoo, Foo>()
+                .AddScoped<IBar>(_ => new Bar())
+                .AddSingleton<IBaz, Baz>()
                 .BuildServiceProvider();
+Debug.Assert(porvider.GetService<IFoo>() is Foo);
+Debug.Assert(porvider.GetService<IBar>() is Bar);
+Debug.Assert(porvider.GetService<IBaz>() is Baz);
